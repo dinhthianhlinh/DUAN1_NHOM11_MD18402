b4e3d1cf15c48a5532d9965fd61d24ff2a4b312e
dinhthianhlinh <127111321+dinhthianhlinh@users.noreply.github.com>
2024-03-12 08:51:54 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/nhom11_duan1/MainActivity.java

--- app/src/main/java/com/example/nhom11_duan1/MainActivity.java
+++ app/src/main/java/com/example/nhom11_duan1/MainActivity.java
@@ -1,19 +1,43 @@
 package com.example.nhom11_duan1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-     //vgfcghbhjboj
-        // linh đẩy lên
-        //linhday làn 2
+     new Handler().postDelayed(new Runnable() {
+         @Override
+         public void run() {
+             FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+             if(firebaseUser == null){
+                 startActivity(new Intent(MainActivity.this,LoginActivity.class));
+             }else{
+                 startActivity(new Intent(MainActivity.this,HomeActivity.class));
+             }
+             finish();
+         }
+     },1000);
     }
 }
