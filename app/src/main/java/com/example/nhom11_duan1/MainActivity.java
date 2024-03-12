cad471e40f2028eb7d2dbdd2f8b5413a3bb0c3bd
dinhthianhlinh <127111321+dinhthianhlinh@users.noreply.github.com>
2024-03-12 08:49:17 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/nhom11_duan1/MainActivity.java

--- app/src/main/java/com/example/nhom11_duan1/MainActivity.java
+++ app/src/main/java/com/example/nhom11_duan1/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+     //vgfcghbhjboj
         // linh đẩy lên
         //linhday làn 2
     }
